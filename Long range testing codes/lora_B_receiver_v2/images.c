009465b71c05a5f43f7bea8f476f88fed84f06e8
Kartis Legend <128057325+LegendofKARTIS@users.noreply.github.com>
2026-05-23 12:10:23 +0530

    Add files via upload

---
+++ Long range testing codes/lora_B_receiver_v2/images.c
@@ -0,0 +1,15 @@
+#include "images.h"
+
+const ext_img_desc_t images[11] = {
+    { "group_icon", &img_group_icon },
+    { "packetIcon", &img_packet_icon },
+    { "settings_icon", &img_settings_icon },
+    { "settings_icon_1", &img_settings_icon_1 },
+    { "signal_icon", &img_signal_icon },
+    { "target", &img_target },
+    { "tower", &img_tower },
+    { "packets", &img_packets },
+    { "clock", &img_clock },
+    { "time", &img_time },
+    { "router", &img_router },
+};
---
+++ Long range testing codes/lora_B_receiver_v2/images.h
@@ -0,0 +1,37 @@
+#ifndef EEZ_LVGL_UI_IMAGES_H
+#define EEZ_LVGL_UI_IMAGES_H
+
+#include <lvgl.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const lv_img_dsc_t img_group_icon;
+extern const lv_img_dsc_t img_packet_icon;
+extern const lv_img_dsc_t img_settings_icon;
+extern const lv_img_dsc_t img_settings_icon_1;
+extern const lv_img_dsc_t img_signal_icon;
+extern const lv_img_dsc_t img_target;
+extern const lv_img_dsc_t img_tower;
+extern const lv_img_dsc_t img_packets;
+extern const lv_img_dsc_t img_clock;
+extern const lv_img_dsc_t img_time;
+extern const lv_img_dsc_t img_router;
+
+#ifndef EXT_IMG_DESC_T
+#define EXT_IMG_DESC_T
+typedef struct _ext_img_desc_t {
+    const char *name;
+    const lv_img_dsc_t *img_dsc;
+} ext_img_desc_t;
+#endif
+
+extern const ext_img_desc_t images[11];
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*EEZ_LVGL_UI_IMAGES_H*/
